370be04b7ecdad0febacfa71a6fef5332074f699
Noa Sundqvist <you@example.com>
2022-11-10 14:02:47 +0000

    Added gray-matter lib for parsing markdown files

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 import Link from "next/link"
+import React, { Component } from "react"
 
+/*export default class extends Component {
+    render () {
+        return (
+            <div>Your Next.js App</div>
+        )
+    }
+}*/
 
 function HomePage() {
     return <div>
