c70bb2806c4a17ffb736f59c0603571c720981ff
Archie <archie.wilkins17@gmail.com>
2022-11-20 15:38:25 +0000

    Finished nav bar pass 1
    * Added admin nav bar switching based on whether is admin (need to add api call to determine is admin)
    * Create layout shell to make it easier to position elements in correct places
    * Added top nav bar with placeholder nav bar

--- pages/help.js
+++ pages/help.js
@@ -1,15 +1,30 @@
 import Link from "next/link"
-import MasterNavBar from "../public/components/masterNavBar.js"
+import MainLayout from "../public/components/mainLayout.js"
+import React from 'react';
 
 
+class HelpPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pageName: 'Help',
+            //Need to change this to get this 
+            //infomation from API
+        };
+    }
 
-function HelpPage() {
-    return <div>
-        <MasterNavBar />
-        <p>Help</p>
+    render() {
+        return <div>
+            <MainLayout pageName={this.state.pageName}>
+                <div className="whiteBackground mt-5">
+                    <p>Help</p>
+                </div>
+            </MainLayout >
 
-        <Link href="/contact"><p>Contact-Us Page</p></Link>
-    </div >
+            <Link href="/contact"><p>Contact-Us Page</p></Link>
+        </div >
+    }
+
 }
 
 export default HelpPage
---
+++ public/components/mainLayout.js
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { loadGetInitialProps } from 'next/dist/shared/lib/utils.js';
+import React from 'react';
+import MasterNavBar from './masterNavBar.js';
+import TopNavBar from './topNavBar.js';
+
+class MainLayout extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            //Need to change this to get this 
+            //infomation from API
+            isAdmin: false,
+        };
+    }
+
+    render() {
+        return <div>
+            <MasterNavBar isAdmin={this.state.isAdmin} />
+            <TopNavBar pageName={this.props.pageName} />
+            <div className="content greyBackground">
+                {this.props.children}
+            </div>
+        </div >
+    }
+
+}
+
+
+export default MainLayout
