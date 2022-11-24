ba7a1294479c32b825977bf53df6fda8a3426131
Noa Sundqvist <you@example.com>
2022-11-24 10:39:58 +0000

    Fixing all directory changes

--- pages/api/enquiryForm.js
+++ pages/api/enquiryForm.js
@@ -15,5 +15,8 @@ export default function handler(req, res) {
 
     // Found the name.
     // Sends a HTTP success code
-    res.status(200).json({ data: `${body.industry} ${body.business} ${body.email} ${body.questions}` })
+    res.status(200).json({data: { industry: `${body.industry}`, business: `${body.business}`, email: `${body.email}`,
+            questions: `${body.questions}`,co2: `${body.co2}`,body: `${body.spending}`,energy: `${body.energy}`,
+            technology: `${body.technology}`, comments: `${body.questions}`
+    }})
 }
