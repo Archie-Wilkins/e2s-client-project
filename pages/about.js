5c7c5aab27a4f08baca373894666c130f3cc34d4
Noa Sundqvist <you@example.com>
2022-11-24 10:52:34 +0000

    Removed out dated componenet from about - Archie

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,10 @@
 import Link from "next/link"
-import NavBar from "../public/components/navBar.js"
 
 function AboutUsPage() {
     return <div>
-        <NavBar />
-        {/* <Link href="/"><p>Home</p>
+        <Link href="/"><p>Home</p>
         </Link>
-        <Link href="/contact"><p>Contact-Us Page</p></Link> */}
+        <Link href="/contact"><p>Contact-Us Page</p></Link>
     </div>
 }
 
