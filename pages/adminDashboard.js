4944d745d8afa9d3a8bc43993a8d5e7791f611cd
Noa Sundqvist <you@example.com>
2022-11-27 19:13:01 +0000

    Added segments for admin hub

--- pages/adminDashboard.js
+++ pages/adminDashboard.js
@@ -6,6 +6,10 @@ function AdminHub(){
             <hr className={"h1-underline"}/>
             <p className={"dashboard-sub-header"}>Welcome to E2S, Admin.</p>
         </div>
+        <div className={"admin-content"}>
+            <div className={"business-section"}></div>
+            <div className={"calendar-section"}></div>
+        </div>
     </div>
 }
 
