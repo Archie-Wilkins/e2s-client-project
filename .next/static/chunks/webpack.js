ce5d706d0e7c8375679dff5e9e563f943074b4ce
Noa Sundqvist <sundqvistn@cardiff.ac.uk>
2022-11-30 15:11:03 +0000

    moved landing page content back into index

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8d74a65f2501c22b"; }
+/******/ 		__webpack_require__.h = function() { return "22d7c62555e03c16"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
