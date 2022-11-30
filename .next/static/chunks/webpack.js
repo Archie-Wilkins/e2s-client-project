813f84da3dfb116b4db7553ce8e95455b737f39d
Noa Sundqvist <sundqvistn@cardiff.ac.uk>
2022-11-30 16:42:41 +0000

    converted admin hub  to a component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a7ed7cca969d4bf0"; }
+/******/ 		__webpack_require__.h = function() { return "7fa2843a035adf99"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/adminDashboardV2.js
+++ pages/adminDashboardV2.js
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Link from "next/link"
-import ToggleSite from ".//toggleSite.js"
 import React from 'react';
 import MainLayout from "../public/components/layouts/MainLayoutShell.js"
 
@@ -23,76 +22,111 @@ class AdminHubV2 extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            businessMockDb {
-                "BusinessDictionary": {
-                    1: {
-                        "businessId": 1,
-                        "businessName": "Mark's Tools",
-                        "industry": "Agriculture",
-                        "director": "Komodo Dragon",
-                    },
-                    2: {
-                        "businessId": 4,
-                        "businessName": "Jakob's Ladder",
-                        "industry": "Furniture",
-                        "Director": "Jason Todd",
-                    },
-                    3: {
-                        "businessId": 20,
-                        "businessName": "Larry's Birds",
-                        "industry": "Pets",
-                        "director": "Michael Jordan",
-                    },
-                    4: {
-                        "businessId": 6,
-                        "businessName": "Ace Chemicals",
-                        "industry": "Chemicals",
-                        "Director": "Jack Napier",
-                    },
-                    5: {
-                        "businessId": 5,
-                        "businessName": "Archie's Hill",
-                        "industry": "Jelly",
-                        "Director": "Noa Sundqvist",
-                    },
-                }
-
-                ,
-            }
+            chosenBusinessRowArray: [],
+            businessRequested: false,
         };
     }
 
-    //
-    // changeSite = () => {
-    //     console.log("change");
-    //     console.log(this.state.displaySiteToggle);
-    //     this.setState({
-    //         displaySiteToggle: !this.state.displaySiteToggle
-    //     })
-    // }
-    //
-    // displayAdminButton = () => {
-    //     if (this.state.displayAdminButton) {
-    //         return <Link className=" navbarLink d-flex" href="/account">
-    //             <FaUsers />
-    //             <p>Admin</p>
-    //         </Link>
-    //     }
-    //     else {
-    //         return null;
-    //     }
-    // }
+
+    handleBusinessRender(businessName){
+
+        this.setState({businessRequested: true});
+
+        for(let i = 0; i < businessName.length;i++){
+            this.state.chosenBusinessRowArray.push(businessName[i].innerText);
+        }
+    }
+
+    handleReturn() {
+        this.setState({chosenBusinessRowArray: []});
+        this.setState({businessRequested: false});
+
+        /*if(!this.state.businessRequested){
+            this.setState({businessRequested: true});
+        }else{
+            this.setState({businessRequested: false});
+        }*/    
+    }
 
     render() {
         return <div>
             <MainLayout isAdmin={true} pageName={"Admin Hub"}>
-                <div className="whiteBackground mt-5">
-                    <h1>ADMIN DASHBOARD</h1>
+            <div className={"admin-header-container"}>
+            <div className={"dashboard-header"}>
+                <h1>ADMIN DASHBOARD</h1>
+                <hr className={"h1-underline"}/>
+                <p className={"dashboard-sub-header"}>Welcome to E2S, Admin.</p><br/>
+                <div className={"admin-content"}>
+                    <div className="row">
+                        <div className="col-sm">
+                            <div className={"business-section"}>
+                                <h3>View All Businesses</h3>
+                                {this.state.businessRequested === false && (
+                                    <table className="table table-hover table-responsive">
+                                        <thead>
+                                        <tr>
+                                            <th scope="col">ID</th>
+                                            <th scope="col">Business Name</th>
+                                            <th scope="col">Industry</th>
+                                            <th scope="col">Director</th>
+                                            <th scope={"col"}>View</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <th name="r1" scope="row">1</th>
+                                            <td name="r1" id={"row1Business"}>Mark's Tools</td>
+                                            <td name={"r1"}>Agriculture</td>
+                                            <td name={"r1"}>Komodo Dragon</td>
+                                            <td name={"r1"}><button onClick={() => this.handleBusinessRender(document.getElementsByName("r1"))}>Here</button></td>
+                                        </tr>
+                                        <tr>
+                                            <th name={"r2"} scope="row">2</th>
+                                            <td name={"r2"}>Jakob's Ladders</td>
+                                            <td name={"r2"}>Furniture</td>
+                                            <td name={"r2"}>Jason Todd</td>
+                                            <td><button onClick={() => this.handleBusinessRender(document.getElementsByName("r2"))}>Here</button></td>
+                                        </tr>
+                                        <tr>
+                                            <th name="r3" scope="row">3</th>
+                                            <td name={"r3"} colSpan={"1"}>Larry's Birds</td>
+                                            <td name={"r3"}>Pets</td>
+                                            <td name={"r3"}>Michael Jordan</td>
+                                            <td><button onClick={() => this.handleBusinessRender(document.getElementsByName("r3"))}>Here</button></td>
+                                        </tr>
+                                        <tr>
+                                            <th name={"r4"} scope="row">4</th>
+                                            <td name={"r4"}>Ace Chemicals</td>
+                                            <td name={"r4"}>Chemicals</td>
+                                            <td name={"r4"}>Jack Napier</td>
+                                            <td><button onClick={() => this.handleBusinessRender(document.getElementsByName("r4"))}>Here</button></td>
+                                        </tr>
+                                        <tr>
+                                            <th name="r5" scope="row">5</th>
+                                            <td name={"r5"}>Ace Chemicals</td>
+                                            <td name={"r5"}>Chemicals</td>
+                                            <td name={"r5"}>Jack Napier</td>
+                                            <td><button onClick={() => this.handleBusinessRender(document.getElementsByName("r5"))}>Here</button></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                )}
+                                {this.state.businessRequested === true && (
+                                    <div>
+                                        <h1>{JSON.stringify(this.state.chosenBusinessRowArray)}</h1>
+                                        <button onClick={() => this.handleReturn()}>Back</button>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    </div><br/>
+                    
                 </div>
-            </MainLayout >
-        </div >
+            </div>
+        </div>
+        </MainLayout>
+        </div>
     }
-
 }
 
 
