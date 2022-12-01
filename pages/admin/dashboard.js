37069f101699a155783057f26dac4cd0441b0e9d
Archie <WilkinsAT1@cardiff.ac.uk>
2022-12-01 14:37:15 +0000

    moved admin dashboard to admin/dashboard and converted in from functional to use ES6

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,74 +1,239 @@
-import Link from "next/link"
-import MainLayout from "../../public/components/layouts/mainLayoutShell.js"
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Link from "next/link";
+import React from "react";
+import MainLayout from "../../public/components/layouts/mainLayoutShell.js";
 
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chosenBusinessRowArray: [],
+      businessRequested: false,
+    };
+  }
 
-class SystemAdmin extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            businessMockDb: {
-                "BusinessDictionary": {
-                    1: {
-                        "businessId": 1,
-                        "businessName": "Mark's Tools",
-                        "industry": "Agriculture",
-                        "director": "Komodo Dragon",
-                    },
-                    2: {
-                        "businessId": 4,
-                        "businessName": "Jakob's Ladder",
-                        "industry": "Furniture",
-                        "Director": "Jason Todd",
-                    },
-                    3: {
-                        "businessId": 20,
-                        "businessName": "Larry's Birds",
-                        "industry": "Pets",
-                        "director": "Michael Jordan",
-                    },
-                    4: {
-                        "businessId": 6,
-                        "businessName": "Ace Chemicals",
-                        "industry": "Chemicals",
-                        "Director": "Jack Napier",
-                    },
-                    5: {
-                        "businessId": 5,
-                        "businessName": "Archie's Hill",
-                        "industry": "Jelly",
-                        "Director": "Noa Sundqvist",
-                    },
-                }
-            },
+  handleBusinessRender(businessName) {
+    this.setState({ businessRequested: true });
 
-            pageName: 'System Admin ',
-            //Would be ideal to store these in some sort of cookie or global variable
-            //if these are not present then the nav bar will default to standard ESM nav bar 
-            isAdmin: true,
-        };
+    for (let i = 0; i < businessName.length; i++) {
+      this.state.chosenBusinessRowArray.push(businessName[i].innerText);
     }
+  }
 
-    displayTableData = () => {
-    //put a function in here to loop through
-    }
+  handleReturn() {
+    this.setState({ chosenBusinessRowArray: [] });
+    this.setState({ businessRequested: false });
+  }
 
-    render() {
-        return <div>
-            <MainLayout isAdmin={this.state.isAdmin}  pageName={this.state.pageName}>
-                <div className="whiteBackground mt-5">
-                    <h1>System Admin </h1>
+  render() {
+    return (
+      <div>
+        <MainLayout isAdmin={true} pageName={"Admin Hub"}>
+          <div className={"admin-header-container"}>
+            <h1 className="dashboard-header">ADMIN DASHBOARD</h1>
+            <hr className={"h1-underline"} />
+            <p className={"dashboard-sub-header"}>Welcome to E2S, Admin.</p>
+            <br />
+            <div className={"admin-content"}>
+              <div className="row">
+                <div className="col-sm">
+                  <div className={"business-section"}>
+                    {this.state.businessRequested === false && (
+                      <div>
+                        <h1>View All Businesses</h1>
+                        <div className="tableHolder">
+                          <table
+                            className="table table-hover table-responsive"
+                            height="100"
+                            data-testid="businessTable"
+                          >
+                            <thead>
+                              <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Business Name</th>
+                                <th scope="col">Industry</th>
+                                <th scope="col">Director</th>
+                                <th scope={"col"}>View</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <th name="r1" scope="row">
+                                  1
+                                </th>
+                                <td name="r1" id={"row1Business"}>
+                                  Mark's Tools
+                                </td>
+                                <td name={"r1"}>Agriculture</td>
+                                <td name={"r1"}>Komodo Dragon</td>
+                                <td name={"r1"}>
+                                  <button
+                                    onClick={() =>
+                                      this.handleBusinessRender(
+                                        document.getElementsByName("r1")
+                                      )
+                                    }
+                                  >
+                                    Here
+                                  </button>
+                                </td>
+                              </tr>
+                              <tr>
+                                <th name={"r2"} scope="row">
+                                  2
+                                </th>
+                                <td name={"r2"}>Jakob's Ladders</td>
+                                <td name={"r2"}>Furniture</td>
+                                <td name={"r2"}>Jason Todd</td>
+                                <td>
+                                  <button
+                                    onClick={() =>
+                                      this.handleBusinessRender(
+                                        document.getElementsByName("r2")
+                                      )
+                                    }
+                                  >
+                                    Here
+                                  </button>
+                                </td>
+                              </tr>
+                              <tr>
+                                <th name="r3" scope="row">
+                                  3
+                                </th>
+                                <td name={"r3"} colSpan={"1"}>
+                                  Larry's Birds
+                                </td>
+                                <td name={"r3"}>Pets</td>
+                                <td name={"r3"}>Michael Jordan</td>
+                                <td>
+                                  <button
+                                    onClick={() =>
+                                      this.handleBusinessRender(
+                                        document.getElementsByName("r3")
+                                      )
+                                    }
+                                  >
+                                    Here
+                                  </button>
+                                </td>
+                              </tr>
+                              <tr>
+                                <th name={"r4"} scope="row">
+                                  4
+                                </th>
+                                <td name={"r4"}>Ace Chemicals</td>
+                                <td name={"r4"}>Chemicals</td>
+                                <td name={"r4"}>Jack Napier</td>
+                                <td>
+                                  <button
+                                    onClick={() =>
+                                      this.handleBusinessRender(
+                                        document.getElementsByName("r4")
+                                      )
+                                    }
+                                  >
+                                    Here
+                                  </button>
+                                </td>
+                              </tr>
+                              <tr>
+                                <th name="r5" scope="row">
+                                  5
+                                </th>
+                                <td name={"r5"}>Ace Chemicals</td>
+                                <td name={"r5"}>Chemicals</td>
+                                <td name={"r5"}>Jack Napier</td>
+                                <td>
+                                  <button
+                                    onClick={() =>
+                                      this.handleBusinessRender(
+                                        document.getElementsByName("r5")
+                                      )
+                                    }
+                                  >
+                                    Here
+                                  </button>
+                                </td>
+                              </tr>
+                              <tr>
+                                <th name="r5" scope="row">
+                                  6
+                                </th>
+                                <td name={"r5"}>Ace Chemicals</td>
+                                <td name={"r5"}>Chemicals</td>
+                                <td name={"r5"}>Jack Napier</td>
+                                <td>
+                                  <button
+                                    onClick={() =>
+                                      this.handleBusinessRender(
+                                        document.getElementsByName("r5")
+                                      )
+                                    }
+                                  >
+                                    Here
+                                  </button>
+                                </td>
+                              </tr>
+                              <tr>
+                                <th name="r5" scope="row">
+                                  7
+                                </th>
+                                <td name={"r5"}>Ace Chemicals</td>
+                                <td name={"r5"}>Chemicals</td>
+                                <td name={"r5"}>Jack Napier</td>
+                                <td>
+                                  <button
+                                    onClick={() =>
+                                      this.handleBusinessRender(
+                                        document.getElementsByName("r5")
+                                      )
+                                    }
+                                  >
+                                    Here
+                                  </button>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    )}
+                    {this.state.businessRequested === true && (
+                      <div classname="businessSummaryBox">
+                        <h1>{this.state.chosenBusinessRowArray[1]}</h1>
+                        <br />
+                        <h3>ID: {this.state.chosenBusinessRowArray[0]}</h3>
+                        <h3>
+                          Industry: {this.state.chosenBusinessRowArray[2]}
+                        </h3>
+                        <h3>CEO: {this.state.chosenBusinessRowArray[3]}</h3>
+                        <br />
+                        <br />
+                        <button onClick={() => this.handleReturn()}>
+                          Back
+                        </button>
+                      </div>
+                    )}
+                  </div>
                 </div>
-
-                <div className="whiteBackground mt-5">
-                    <h1>System Admin </h1>
+              </div>
+              <br />
+              <br />
+              <div className="row">
+                <div className="col-sm">
+                  <h1>ONBOARDING</h1>
+                  <p>
+                    To onboard a new client go <Link href="#"> here</Link>
+                  </p>
                 </div>
-
-
-            </MainLayout >
-        </div >
-    }
-
+              </div>
+            </div>
+          </div>
+        </MainLayout>
+      </div>
+    );
+  }
 }
 
-export default SystemAdmin
+export default Dashboard;
--- pages/adminDashboardV2.js
+++
@@ -1,9 +0,0 @@
-import AdminDashboardComponent from "../public/components/adminDashboardComponent.js"
-
-function LoginPage() {
-    return <div className="AdminDashboardBackground">
-        <AdminDashboardComponent />
-    </div>
-}
-
-export default LoginPage
