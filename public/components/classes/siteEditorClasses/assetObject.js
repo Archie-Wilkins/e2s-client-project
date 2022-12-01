17ccc9cc48e0d54960063e11826a381e3dc2d7e5
Brodie <beavenb@cardiff.ac.uk>
2022-12-01 16:53:17 +0000

    Setting up object structure for rooms, sites and floors /w getters and setters

--- public/components/classes/siteEditorClasses/assetObject.js
+++ public/components/classes/siteEditorClasses/assetObject.js
@@ -1,11 +1,66 @@
 class AssetObject{
-    constructor(name, path, type, size, dateCreated, dateModified, tags){
+    constructor(tempId, name, typeId, energyEfficiency, energyDemand, emissions){
+
+        // Somehow validate length and type of these for the database input
+        this.tempId = tempId;
         this.name = name;
-        this.path = path;
-        this.type = type;
-        this.size = size;
-        this.dateCreated = dateCreated;
-        this.dateModified = dateModified;
-        this.tags = tags;
+        this.typeId = type;
+        this.energyEfficiency = energyEfficiency;
+        this.energyDemand = energyDemand;
+        this.emissions = emissions;
+
+    }
+
+    getJsonFormat() {
+        return {
+            name: this.name,
+            typeId: this.typeId,
+            energyEfficiency: this.energyEfficiency,
+            energyDemand: this.energyDemand,
+            emissions: this.emissions
+        }
+    }
+
+
+    // Getters and Setters
+
+    getName() {
+        return this.name;
+    }
+
+    getTypeId() {
+        return this.typeId;
+    }
+
+    getEnergyEfficiency() {
+        return this.energyEfficiency;
+    }
+
+    getEnergyDemand() {
+        return this.energyDemand;
+    }
+
+    getEmissions() {
+        return this.emissions;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    setTypeId(typeId) {
+        this.typeId = typeId;
+    }
+
+    setEnergyEfficiency(energyEfficiency) {
+        this.energyEfficiency = energyEfficiency;
+    }
+
+    setEnergyDemand(energyDemand) {
+        this.energyDemand = energyDemand;
+    }
+
+    setEmissions(emissions) {
+        this.emissions = emissions;
     }
 }
--- public/components/classes/siteEditorClasses/floorObject.js
+++ public/components/classes/siteEditorClasses/floorObject.js
@@ -1,17 +1,61 @@
 class FloorObject{
-    constructor(id, name, image, width, height, x, y, z, rotation, floor, type, isLocked, isHidden){
-        this.id = id;
+    constructor(tempId, name, floorNumber, floorSizeX, floorSizeY){
+        this.tempId = tempId;
         this.name = name;
-        this.image = image;
-        this.width = width;
-        this.height = height;
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.rotation = rotation;
-        this.floor = floor;
-        this.type = type;
-        this.isLocked = isLocked;
-        this.isHidden = isHidden;
+        this.floorNumber = floorNumber;
+        this.floorSizeX= floorSizeX;
+        this.floorSizeY = floorSizeY;
+        this.rooms = []
+    }
+
+    getJsonFormat() {
+        return {
+            name: this.name,
+            floorNumber: this.floorNumber,
+            floorSizeX: this.floorSizeX,
+            floorSizeY: this.floorSizeY,
+        }
+    }
+
+    addRoomObject(roomObject) {
+        this.rooms.push(roomObject);
+    }
+
+    // Getters and Setters
+
+    getRooms() {
+        return this.rooms;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getFloorNumber() {
+        return this.floorNumber;
+    }
+
+    getFloorSizeX() {
+        return this.floorSizeX;
+    }
+
+    getFloorSizeY() {
+        return this.floorSizeY;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    setFloorNumber(floorNumber) {
+        this.floorNumber = floorNumber;
+    }
+
+    setFloorSizeX(floorSizeX) {
+        this.floorSizeX = floorSizeX;
+    }
+
+    setFloorSizeY(floorSizeY) {
+        this.floorSizeY = floorSizeY;
     }
 }
