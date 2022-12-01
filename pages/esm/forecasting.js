268aea0a1cae8a97863e6da6bb0ab6fc32b27154
Archie <WilkinsAT1@cardiff.ac.uk>
2022-12-01 00:46:19 +0000

    Graph works and updates with button press - needs tidying

--- pages/esm/forecasting.js
+++ pages/esm/forecasting.js
@@ -1,157 +1,168 @@
-import Link from "next/link"
-import MainLayout from "../../public/components/layouts/mainLayoutShell.js"
-import React from 'react';
-import { LineChart, Line, Label, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
-
-
+import Link from "next/link";
+import MainLayout from "../../public/components/layouts/mainLayoutShell.js";
+import EnergyCostForecastGraph from "../../public/components/forecastingGraphs/energyCostForecastGraph";
+import React from "react";
+import {
+  LineChart,
+  Line,
+  Label,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 class Forecasting extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            pageName: 'Forecasting',
-            //Need to change this to get this 
-            //infomation from API
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageName: "Forecasting",
+      //Need to change this to get this
+      //infomation from API
 
-            //Would be ideal to store these in some sort of cookie or global variable
-            //if these are not present then the nav bar will default to standard ESM nav bar 
-        
-            isDirector: false,
-            
-            
-            weeklyData: [
-                {name: 'Mon 5th', cost: 400, c02: 153},
-                {name: 'Tue 6th', cost: 415, c02: 145},
-                {name: 'Wed 7th', cost: 460, c02: 131},
-                {name: 'Thu 8th', cost: 501, c02: 115},
-                {name: 'Fri 9th', cost: 446, c02: 165},
-                {name: 'Sat 10th', cost: 608, c02: 135},
-                {name: 'Sun 11th', cost: 608, c02: 135},
-            ],
-                   
-            monthlyData: [
-                {name: '14/11/2022', cost: 400, c02: 153},
-                {name: '15/11/2022', cost: 415, c02: 145},
-                {name: '16/11/2022', cost: 460, c02: 131},
-                {name: '17/11/2022', cost: 501, c02: 115},
-                {name: '18/11/2022', cost: 446, c02: 165},
-                {name: '19/11/2022', cost: 608, c02: 135},
-                {name: '20/11/2022', cost: 562, c02: 115},
-                {name: '21/11/2022', cost: 531, c02: 112},
-                {name: '22/11/2022', cost: 558, c02: 113},
-                {name: '23/11/2022', cost: 515, c02: 109},
-                {name: '24/11/2022', cost: 589, c02: 106},
-                {name: '25/11/2022', cost: 605, c02: 105},
-                {name: '26/11/2022', cost: 601, c02: 103},
-                {name: '27/11/2022', cost: 561, c02: 101},
-                {name: '28/11/2022', cost: 410, c02: 103},
-                {name: '29/11/2022', cost: 440, c02: 111},
-                {name: '30/11/2022', cost: 495, c02: 121},
-                {name: '1/12/2022', cost: 411, c02: 136},
-                {name: '2/12/2022', cost: 452, c02: 141},
-                {name: '3/12/2022', cost: 431, c02: 145},
-                {name: '4/12/2022', cost: 459, c02: 131},
-                {name: '5/12/2022', cost: 473, c02: 113},
-                {name: '6/12/2022', cost: 413, c02: 121},
-                {name: '7/12/2022', cost: 519, c02: 117},
-                {name: '8/12/2022', cost: 511, c02: 113},
-                {name: '9/12/2022', cost: 506, c02: 121},
-                {name: '10/12/2022', cost: 520, c02: 119},
-                {name: '11/12/2022', cost: 549, c02: 131},
-                {name: '12/12/2022', cost: 560, c02: 136},
-                {name: '13/12/2022', cost: 580, c02: 145},
-                {name: '14/12/2022', cost: 600, c02: 145},
-            ],
+      //Would be ideal to store these in some sort of cookie or global variable
+      //if these are not present then the nav bar will default to standard ESM nav bar
 
+      isDirector: false,
 
-        };
-    }
+      weeklyData: [
+        { name: "Mon 5th", cost: 400, c02: 153 },
+        { name: "Tue 6th", cost: 415, c02: 145 },
+        { name: "Wed 7th", cost: 460, c02: 131 },
+        { name: "Thu 8th", cost: 501, c02: 115 },
+        { name: "Fri 9th", cost: 446, c02: 165 },
+        { name: "Sat 10th", cost: 608, c02: 135 },
+        { name: "Sun 11th", cost: 608, c02: 135 },
+      ],
 
-    render() {
-        return <div>
-            <MainLayout isDirector={this.state.isDirector} pageName={this.state.pageName}>
-                
-                {/* Energy Cost */}
-                <div className="container d-flex flex-column align-items-center w-100">
-                         
-                    <div className="row mt-5 w-75">
-                        <div className="col-lg rounded text-center">
-                            <h2>Energy Cost</h2>
-                        </div>
-                    </div>
+      monthlyData: [
+        { name: "14/11/2022", cost: 400, c02: 153 },
+        { name: "15/11/2022", cost: 415, c02: 145 },
+        { name: "16/11/2022", cost: 460, c02: 131 },
+        { name: "17/11/2022", cost: 501, c02: 115 },
+        { name: "18/11/2022", cost: 446, c02: 165 },
+        { name: "19/11/2022", cost: 608, c02: 135 },
+        { name: "20/11/2022", cost: 562, c02: 115 },
+        { name: "21/11/2022", cost: 531, c02: 112 },
+        { name: "22/11/2022", cost: 558, c02: 113 },
+        { name: "23/11/2022", cost: 515, c02: 109 },
+        { name: "24/11/2022", cost: 589, c02: 106 },
+        { name: "25/11/2022", cost: 605, c02: 105 },
+        { name: "26/11/2022", cost: 601, c02: 103 },
+        { name: "27/11/2022", cost: 561, c02: 101 },
+        { name: "28/11/2022", cost: 410, c02: 103 },
+        { name: "29/11/2022", cost: 440, c02: 111 },
+        { name: "30/11/2022", cost: 495, c02: 121 },
+        { name: "1/12/2022", cost: 411, c02: 136 },
+        { name: "2/12/2022", cost: 452, c02: 141 },
+        { name: "3/12/2022", cost: 431, c02: 145 },
+        { name: "4/12/2022", cost: 459, c02: 131 },
+        { name: "5/12/2022", cost: 473, c02: 113 },
+        { name: "6/12/2022", cost: 413, c02: 121 },
+        { name: "7/12/2022", cost: 519, c02: 117 },
+        { name: "8/12/2022", cost: 511, c02: 113 },
+        { name: "9/12/2022", cost: 506, c02: 121 },
+        { name: "10/12/2022", cost: 520, c02: 119 },
+        { name: "11/12/2022", cost: 549, c02: 131 },
+        { name: "12/12/2022", cost: 560, c02: 136 },
+        { name: "13/12/2022", cost: 580, c02: 145 },
+        { name: "14/12/2022", cost: 600, c02: 145 },
+      ],
 
-                    <div className="row h-100 w-75">
-                        <div className="col p-2 whiteBackground rounded vh-70">
-                            {/* <h3>Graph goes here</h3> */}
-                        <ResponsiveContainer>
-                            <LineChart data={this.state.monthlyData}>
-                                <Line type="monotone" dataKey="cost" stroke="#8884d8" />
-                                <XAxis dataKey="name">
-                                    <Label value="Date" offset={-5} position="insideBottom" />
-                                </XAxis>
-                                <YAxis dataKey="cost" >
-                                    <Label value="Cost (£)" angle={270}  offset={-5} position="left" />
-                                </YAxis>
-                                <Tooltip />
-                            </LineChart>
-                            </ResponsiveContainer>
+      yearlyData: [
+        { name: "December", cost: 400, c02: 153 },
+        { name: "January", cost: 415, c02: 145 },
+        { name: "February", cost: 460, c02: 131 },
+        { name: "March", cost: 501, c02: 115 },
+        { name: "April", cost: 446, c02: 165 },
+        { name: "May", cost: 608, c02: 135 },
+        { name: "June", cost: 608, c02: 138 },
+        { name: "July", cost: 620, c02: 165 },
+        { name: "August", cost: 630, c02: 175 },
+        { name: "September", cost: 640, c02: 185 },
+        { name: "October", cost: 645, c02: 175 },
+        { name: "November", cost: 633, c02: 165 },
+        { name: "December", cost: 640, c02: 165 },
+      ],
+    };
+  }
 
-                        </div>
-                    </div>
+  render() {
+    return (
+      <div>
+        <MainLayout
+          isDirector={this.state.isDirector}
+          pageName={this.state.pageName}
+        >
+          {/* Energy Cost */}
+          <div className="container d-flex flex-column align-items-center w-100">
+            <div className="row mt-5 w-75">
+              <div className="col-lg rounded text-center">
+                <h2>Energy Cost</h2>
+              </div>
+            </div>
 
-                    <div className="row mt-2 g-2 w-75">
-                            <div className="col whiteBackground rounded mr-1">Box 1</div>
-                            <div className="col whiteBackground rounded mx-1">Box 1</div>
-                            <div className="col whiteBackground rounded ml-1">Box 1</div>
-                    </div>
-                </div>
-                
-            
-                {/* Energy Usage */}
-                <div className="container d-flex flex-column align-items-center w-100">
-                         
-                    <div className="row mt-5 w-75">
-                        <div className="col-lg rounded text-center">
-                            <h2>C02 Emissions</h2>
-                        </div>
-                    </div>
+            <EnergyCostForecastGraph
+              weeklyData={this.state.weeklyData}
+              monthlyData={this.state.monthlyData}
+              yearlyData={this.state.yearlyData}
+            />
 
-                    <div className="row h-100 w-75">
-                        <div className="col p-2 whiteBackground rounded vh-70">
-                            {/* <h3>Graph goes here</h3> */}
-                        <ResponsiveContainer>
-                            <LineChart data={this.state.monthlyData}>
-                                <Line type="monotone" dataKey="c02" stroke="#8884d8" />
-                                <XAxis dataKey="name">
-                                    <Label value="Date" offset={-5} position="insideBottom" />
-                                </XAxis>
-                                <YAxis dataKey="c02" >
-                                    <Label value="C02 Emissions (Kg)" angle={270}  offset={-5} position="left" />
-                                </YAxis>
-                                <Tooltip />
-                            </LineChart>
-                            </ResponsiveContainer>
+            <div className="row mt-2 g-2 w-75">
+              <div className="col whiteBackground rounded mr-1">Box 1</div>
+              <div className="col whiteBackground rounded mx-1">Box 1</div>
+              <div className="col whiteBackground rounded ml-1">Box 1</div>
+            </div>
+          </div>
 
-                        </div>
-                    </div>
+          {/* Energy Usage */}
+          <div className="container d-flex flex-column align-items-center w-100">
+            <div className="row mt-5 w-75">
+              <div className="col-lg rounded text-center">
+                <h2>C02 Emissions</h2>
+              </div>
+            </div>
 
-                    <div className="row mt-2 g-2 w-75">
-                            <div className="col whiteBackground rounded mr-1">Box 1</div>
-                            <div className="col whiteBackground rounded mx-1">Box 1</div>
-                            <div className="col whiteBackground rounded ml-1">Box 1</div>
-                    </div>
-                </div>
+            <div className="row h-100 w-75">
+              <div className="col p-2 whiteBackground rounded vh-70">
+                {/* <h3>Graph goes here</h3> */}
+                <ResponsiveContainer>
+                  <LineChart data={this.state.monthlyData}>
+                    <Line type="monotone" dataKey="c02" stroke="#8884d8" />
+                    <XAxis dataKey="name">
+                      <Label value="Date" offset={-5} position="insideBottom" />
+                    </XAxis>
+                    <YAxis dataKey="c02">
+                      <Label
+                        value="C02 Emissions (Kg)"
+                        angle={270}
+                        offset={-5}
+                        position="left"
+                      />
+                    </YAxis>
+                    <Tooltip />
+                  </LineChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
 
-                {/* Energy Usage */}
-                
-                
+            <div className="row mt-2 g-2 w-75">
+              <div className="col whiteBackground rounded mr-1">Box 1</div>
+              <div className="col whiteBackground rounded mx-1">Box 1</div>
+              <div className="col whiteBackground rounded ml-1">Box 1</div>
+            </div>
+          </div>
 
-            </MainLayout >
+          {/* Energy Usage */}
+        </MainLayout>
 
-            <Link href="/contact"><p>Contact-Us Page</p></Link>
-        </div >
-    }
-
+        <Link href="/contact">
+          <p>Contact-Us Page</p>
+        </Link>
+      </div>
+    );
+  }
 }
 
-export default Forecasting
+export default Forecasting;
--- public/components/forecastingGraphs/energyCostForecastGraph.js
+++ public/components/forecastingGraphs/energyCostForecastGraph.js
@@ -1,61 +1,150 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Link from "next/link";
 import React from "react";
-
-// React Icons
-import { FaHandshake } from "react-icons/fa";
-import { FaTachometerAlt } from "react-icons/fa";
-import { FaHouseUser } from "react-icons/fa";
-import { FaUsers } from "react-icons/fa";
+import {
+  LineChart,
+  Line,
+  Label,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 class EnergyCostForecastGraph extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentGraph: this.displayGraph(1),
+    };
   }
+
+  //   componentDidMount() {
+  //     this.setState({ currentGraph: "this.displayWeeklyGraph()" });
+  //   }
+
+  displayGraph = (graphNumber) => {
+    console.log("Yes" + graphNumber);
+    switch (graphNumber) {
+      case 1:
+        console.log("Yes" + "displayWeeklyGraph");
+        this.setState({
+          currentGraph: this.displayWeeklyGraph(),
+        });
+        return this.displayWeeklyGraph();
+        break;
+      case 2:
+        console.log("Yes" + "displayMonthlyGraph");
+        this.setState({
+          currentGraph: this.displayMonthlyGraph(),
+        });
+        break;
+      case 3:
+        console.log("Yes" + "displayYearlyGraph");
+        this.setState({
+          currentGraph: this.displayYearlyGraph(),
+        });
+        break;
+      default:
+        this.setState({
+          currentGraph: this.displayWeeklyGraph(),
+        });
+    }
+  };
+
+  displayWeeklyGraph = () => {
+    console.log("1");
+    return (
+      <div className="w-100 h-100">
+        <ResponsiveContainer>
+          <LineChart data={this.props.weeklyData}>
+            <Line type="monotone" dataKey="cost" stroke="#8884d8" />
+            <XAxis dataKey="name">
+              <Label value="Date" offset={-5} position="insideBottom" />
+            </XAxis>
+            <YAxis dataKey="cost">
+              <Label value="Cost (£)" angle={270} offset={-5} position="left" />
+            </YAxis>
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    );
+  };
+
+  displayMonthlyGraph = () => {
+    console.log("2");
+    return (
+      <div className="w-100 h-100">
+        <ResponsiveContainer>
+          <LineChart data={this.props.monthlyData}>
+            <Line type="monotone" dataKey="cost" stroke="#8884d8" />
+            <XAxis dataKey="name">
+              <Label value="Date" offset={-5} position="insideBottom" />
+            </XAxis>
+            <YAxis dataKey="cost">
+              <Label value="Cost (£)" angle={270} offset={-5} position="left" />
+            </YAxis>
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    );
+  };
+
+  displayYearlyGraph = () => {
+    console.log("3");
+    return (
+      <div className="w-100 h-100">
+        <ResponsiveContainer>
+          <LineChart data={this.props.yearlyData}>
+            <Line type="monotone" dataKey="cost" stroke="#8884d8" />
+            <XAxis dataKey="name">
+              <Label value="Date" offset={-5} position="insideBottom" />
+            </XAxis>
+            <YAxis dataKey="cost">
+              <Label value="Cost (£)" angle={270} offset={-5} position="left" />
+            </YAxis>
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    );
+  };
 
   render() {
     return (
-      <div>
-        {/* Energy Cost */}
-        <div className="container d-flex flex-column align-items-center w-100">
-          <div className="row mt-5 w-75">
-            <div className="col-lg rounded text-center">
-              <h2>Energy Cost</h2>
-            </div>
+      <div className="row h-100 w-75">
+        <div className="col p-2 whiteBackground rounded vh-70">
+          <div className="blueBackground rounded">
+            <button
+              onClick={() => {
+                this.displayGraph(1);
+              }}
+            >
+              Weekly
+            </button>
+            <button
+              onClick={() => {
+                this.displayGraph(2);
+              }}
+            >
+              Monthly
+            </button>
+            <button
+              onClick={() => {
+                this.displayGraph(3);
+              }}
+            >
+              Yearly
+            </button>
           </div>
 
-          <div className="row h-100 w-75">
-            <div className="col p-2 whiteBackground rounded vh-70">
-              <ResponsiveContainer>
-                <LineChart data={this.state.monthlyData}>
-                  <Line type="monotone" dataKey="cost" stroke="#8884d8" />
-                  <XAxis dataKey="name">
-                    <Label value="Date" offset={-5} position="insideBottom" />
-                  </XAxis>
-                  <YAxis dataKey="cost">
-                    <Label
-                      value="Cost (£)"
-                      angle={270}
-                      offset={-5}
-                      position="left"
-                    />
-                  </YAxis>
-                  <Tooltip />
-                </LineChart>
-              </ResponsiveContainer>
-            </div>
-          </div>
-
-          <div className="row mt-2 g-2 w-75">
-            <div className="col whiteBackground rounded mr-1">Box 1</div>
-            <div className="col whiteBackground rounded mx-1">Box 1</div>
-            <div className="col whiteBackground rounded ml-1">Box 1</div>
-          </div>
+          {this.state.currentGraph}
         </div>
       </div>
     );
   }
 }
 
-export default energyCostForecastGraph;
+export default EnergyCostForecastGraph;
