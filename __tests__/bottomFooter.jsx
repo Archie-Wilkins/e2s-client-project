0314a4f9745c601c8974aff809f1a35a814564a2
Archie <archie.wilkins17@gmail.com>
2022-12-07 12:49:39 +0000

    bottom nav bar testa

---
+++ __tests__/bottomFooter.jsx
@@ -0,0 +1,17 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import '@babel/preset-react'
+import React from 'react'
+import BottomFooter from "../public/components/layouts/bottomFooter";
+
+
+describe('bottomFooter', () => {
+    beforeEach(() => {
+        render(<BottomFooter />)
+    })
+
+    it('hasCopyRight', () => {
+        const copyRight = screen.getByText("© Empowering Energy Solutions 2022")
+        expect(copyRight).toBeInTheDocument()
+    })
+})
