629aaa7526c4ca4e00770cb6e4aedef4da544c94
c2064724 <allen-harrise@cardiff.ac.uk>
2022-12-08 09:30:34 +0000

    moved accountView to accountPage, can now logout, added aria labels

--- pages/account.js
+++ pages/account.js
@@ -1,13 +1,40 @@
 import Link from "next/link"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {useRouter} from 'next/router'
+import React from "react";
+import Cookies from "js-cookie";
 
-function AccountPage() {
-    return <div aria-label="account page content">
-        <Link href="/"><p>Home</p></Link>
-        <Link href="/contact"><p>Contact Us Page</p></Link>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-    </div>
+class AccountPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            userID: ""
+        };
+    }
+
+    logout = async (event) => {
+        Cookies.remove("user");
+        window.location = "/login";
+    }
+
+
+    render() {
+        return <div className="loginBackground" aria-label="account page">
+            <div className="account-container" aria-label="account page content">
+                <h2>Account</h2>
+                <div className="line"></div>
+                <div className="account-profile-picture-container" aria-label="account profile picture">
+                    <img style={{width: "150px", height: "150px"}} src={"/_next/static/media/profilePic.fd0cd22e.png"}/>
+                    {/*<img src={profilePic}/>*/}
+                </div>
+                {/*<img>sample profile picture</img>*/}
+                <button onClick={this.logout} aria-label="logout button">logout</button>
+            </div>
+
+        </div>
+    }
 }
 
 export default AccountPage
