3727104e834611439fc47983c45be7da6c97868b
Noa Sundqvist <sundqvistn@cardiff.ac.uk>
2022-12-08 14:38:44 +0000

    Function now stores a user's submitted month and year for invoice, with no option to specify a given day

--- pages/billValidation.js
+++ pages/billValidation.js
@@ -12,13 +12,26 @@ class BillValidation extends React.Component {
             isAdmin: true,
             isDirector: false,
             pageName: "Bill Validation",
-            selectedStartDate: null,
+            selectedYear: "2012",
+            selectedMonth: "january",
+            currentlyCalculating: false,
+            dateSubmitted: false,
+            dateIsValid: false,
         };
     }
 
-    logValue = async (event) => {
-        console.log(this.state.selectedStartDate);
+    submitDate = async (event) => {
+        console.log(this.state.selectedYear);
+        console.log(this.state.selectedMonth);
+        this.setState({dateSubmitted: true});
+        this.setState({currentlyCalculating: true});
+        this.setState({currentlyCalculating: false});
       }
+
+    updateValue = async (event) => {
+        this.setState({selectedMonth: document.getElementById("monthStuff").value});
+        this.setState({selectedYear: document.getElementById("yearStuff").value});
+    }  
 
     render() {
         return ( <div onLoad={this.checkUser} onMouseEnter={this.checkUser} aria-label="Bill validation page">
@@ -35,21 +48,58 @@ class BillValidation extends React.Component {
                 </div>
                 <div aria-label="Invoice data section">
                     <div>
-                        <h3></h3>
-                        <select
-                            value={this.state.selectedStartDate}
-                            onChange={(e) => {
-                            this.setState({selectedStartDate: e.target.value});
-                            }}
-                        >
-                            <option value="a">a</option>
-                            <option value="b">b</option>
-                            <option value="c">c</option>
-                            <option value="d">d</option>
+                        <h3>SELECT THE START DATE FOR THE INVOICE</h3>
+                        
+                        {/*Make dyanmic so that it auto updates in later years */}
+                        <select id="yearStuff" onChange={this.updateValue}>
+                            <option yearValue="2012">2012</option>
+                            <option yearValue="2013">2013</option>
+                            <option yearValue="2014">2014</option>
+                            <option yearValue="2015">2015</option>
+                            <option yearValue="2016">2016</option>
+                            <option yearValue="2017">2017</option>
+                            <option yearValue="2018">2018</option>
+                            <option yearValue="2019">2019</option>
+                            <option yearValue="2020">2020</option>
+                            <option yearValue="2021">2021</option>
+                            <option yearValue="2022">2022</option>
+                            {/*Add a check for current year using external server */}
+                        </select>
+                        
+                        <select id="monthStuff" onChange={this.updateValue}>
+                            <option monthValue="january">JANUARY</option>
+                            <option monthValue="february">FEBRUARY</option>
+                            <option monthValue="march">MARCH</option>
+                            <option monthValue="april">APRIL</option>
+                            <option monthValue="may">MAY</option>
+                            <option monthValue="june">JUNE</option>
+                            <option monthValue="july">JULY</option>
+                            <option monthValue="august">AUGUST</option>
+                            <option monthValue="septmeber">SEPTEMBER</option>
+                            <option monthValue="october">OCTOBER</option>
+                            <option monthValue="november">NOVEMBER</option>
+                            <option monthValue="december">DECEMBER</option>
                         </select>
 
-                        <button onClick={this.logValue}>submit</button>
+                        {/*Initially we will check only for 31 days*/}
+
+                        <button onClick={this.submitDate}>submit</button>
                     </div>
+
+                    <div>
+                        {this.state.dateSubmitted &&(
+                            <div>
+                                {this.state.currentlyCalculating &&(
+                                    <p>VALIDATING....</p>
+                                )}
+                                {!this.state.currentlyCalculating &&(
+                                    <p>CHECKED</p>
+                                )}
+                            </div>
+                        )}
+                    </div>
+
+
                     <div>
                         <h3></h3>
                     </div>
