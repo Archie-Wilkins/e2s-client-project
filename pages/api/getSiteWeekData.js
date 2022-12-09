34ac6e7087be2e26836e6defb68903067fdb5400
Archie <archie.wilkins17@gmail.com>
2022-12-09 14:10:04 +0000

    removed some dead code and tried to fix a test warning

--- pages/api/getSiteWeekData.js
+++ pages/api/getSiteWeekData.js
@@ -27,7 +27,4 @@ export default async function handler(req, res) {
         console.log(e);
         return res.status(500);
     }
-    // Found the name.
-    // Sends a HTTP success code
-    res.status(200);
 }
