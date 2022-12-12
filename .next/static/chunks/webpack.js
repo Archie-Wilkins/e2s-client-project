fdd437281c4515f03ec57fe319fa99c218be05fe
Archie <archie.wilkins17@gmail.com>
2022-12-12 15:46:43 +0000

    fixed top nav bar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d08b596e3499f8e5"; }
+/******/ 		__webpack_require__.h = function() { return "cc008a04c518aeb7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- public/components/layouts/publicTopNav.js
+++ public/components/layouts/publicTopNav.js
@@ -17,10 +17,9 @@ class publicTopNav extends React.Component {
                 <div>
                     <Link className="lightLink m-0" href="/"><h4 className="m-0">E<span className='accentText'>2</span>S</h4></Link>
                 </div>
-                <div className="vw-40 d-flex justify-content-around align-items-center">
-                    <Link className="lightLink m-0" href="/#aboutUs"><p className="m-0">About Us</p></Link>
-                    <Link className="lightLink m-0" href="/requestData"><p className="m-0">Request Data</p></Link>
-                    <Link className="lightLink m-0" href="/login"><p className="m-0">Login</p></Link>
+                <div className="w-25 d-flex justify-content-around align-items-center">
+                    <Link className="lightLink m-0 mr-1" href="/requestData"><p className="m-0">Request Data</p></Link>
+                    <Link className="lightLink m-0 mr-1" href="/login"><p className="m-0">Login</p></Link>
                     <Link className="accentButton px-2 m-0" href="/#enquiryForm"><p className="m-0">Join Us</p></Link>
                 </div>
             </div>
