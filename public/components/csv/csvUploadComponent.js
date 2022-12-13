2bf69ea415d7f1d1feabe572fd1d97d96f30128d
Noa Sundqvist <sundqvistn@cardiff.ac.uk>
2022-12-13 05:04:55 +0000

    Updated API routes for uploading data.

--- public/components/csv/csvUploadComponent.js
+++ public/components/csv/csvUploadComponent.js
@@ -80,14 +80,10 @@ class CsvUploadComponent extends React.Component {
       }
     }
 
-    /*const link = document.querySelector("a");
-    const textBlob = new Blob([tableArray], { type: "text/csv" });
-    link.setAttribute("href", URL.createObjectURL(textBlob));
-    link.setAttribute("download", localStorage.getItem("userArrName"));*/
+
     this.setState({ csvData: tableArray });
     this.setState({ csvFileName: localStorage.getItem("userArrName") });
-    /*document.getElementsByClassName("downloadCsvDataButton")[0].style.display =
-      "block";*/
+
   };
 
   returnHome = async (event) => {
@@ -99,24 +95,7 @@ class CsvUploadComponent extends React.Component {
   pushAllSitesToDatabaseApi = async (event) => {
     try {
       // API endpoint where we send form data.
-      const endpoint = "/api/uploadAllHistoricalSiteDataApi";
-
-      // Get data from the form.
-      /*const data = {
-        date: this.state.csvData[x][0],
-        chp1Egen: this.state.csvData[1],
-        chp2Egen: this.state.csvData[2],
-        chp1HeatGen: this.state.csvData[3],
-        chp2HeatGen: this.state.csvData[4],
-        boilerHeat: this.state.csvData[5],
-        feelsLike: this.state.csvData[6],
-        windSpeed : this.state.csvData[7],
-        siteElectrictyDemand: this.state.csvData[8],
-        day_ahead_price: this.state.csvData[9],
-        siteHeatDemand: this.state.csvData[10],
-        importElec: this.state.csvData[11],
-        exportElec: this.state.csvData[12],
-      }*/
+      const endpoint = "/api/site/uploadAllHistoricalSiteDataApi";
 
       let data = [];
 
@@ -166,9 +145,7 @@ class CsvUploadComponent extends React.Component {
       <div aria-label="upload csv page">
         <div className="m-5 p-5 bg-light rounded d-flex flex-column align-items-center justify-content-around ">
           <h1>Upload CSV</h1>
-          <a className="downloadCsvDataButton" aria-label="download csv data button">
-            Download ({this.state.csvFileName})
-          </a>
+
           {!this.state.csvUploaded && (
             <div className="d-flex flex-column align-items-center justify-content-around ">
               <input
@@ -201,30 +178,6 @@ class CsvUploadComponent extends React.Component {
           {this.state.csvUploaded === true && (
             <div aria-label="uploaded file summary page">
               <button onClick={this.returnHome} aria-label="go back button">Back</button>
-
-              {/*
-              <h2>Yor Uploaded Data ({this.state.csvFileName})</h2>
-              <ul>
-                {this.state.csvData.map((name) => (
-                  <div aria-label="file data section">
-                    <h1>Data</h1>
-                    <li>Date: {name[0]}</li>
-                    <li>CHP1_Electricity_Generation_kW: {name[1]}</li>
-                    <li>CHP2_Electricity_Generation_kW: {name[2]}</li>
-                    <li>CHP1_Heat_Output_kW: {name[3]}</li>
-                    <li>CHP2_Heat_Output_kW: {name[4]}</li>
-                    <li>Boiler_Heat_Output_kW: {name[5]}</li>
-                    <li>Feels_Like_Temperature_Deg_C: {name[6]}</li>
-                    <li>Wind_Speed_ms: {name[7]}</li>
-                    <li>Site_Electricity_Demand_kW: {name[8]}</li>
-                    <li>Day_ahead_UK_electricity_price: {name[9]}</li>
-                    <li>Site_Heat_Demand_kW: {name[10]}</li>
-                    <li>Import_Electricity_kW: {name[11]}</li>
-                    <li>Export_Electricity_kWL {name[12]}</li>
-
-                  </div>
-                ))}
-                </ul>*/}
             </div>
           )}
         </div>
